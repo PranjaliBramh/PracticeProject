4c132d2f1881a796a0cc081697e6b44c06a96a48
PranjaliBramh <bramhankarpranjali@gmail.com>
2025-04-21 18:12:33 +0530

    first commit

---
+++ src/main/java/com/example/demo/git/tezt.java
@@ -0,0 +1,19 @@
+package com.example.demo.git;
+
+import java.util.Arrays;
+
+public class tezt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int[] numbers = {5, 2, 8, 1, 3};
+
+	       
+	        Arrays.sort(numbers);
+
+	        
+	        System.out.println("Sorted array: " + Arrays.toString(numbers));
+	}
+
+}
---
+++ src/test/java/com/example/demo/git/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
